2f6b3703906d2a216d84a0fefb08c2f3f3361671
ChiaZen <108150096+ChiaZen@users.noreply.github.com>
2025-07-21 12:15:22 +0200

    style: correct grammatical mistake

--- manage.py
+++ manage.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+
 import os
 import sys
-
 
 def main():
     """Run administrative tasks."""
@@ -12,8 +10,8 @@ def main():
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "available on your PYTHONPATH environment variable? Check if you "
+            "forgot to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
 
